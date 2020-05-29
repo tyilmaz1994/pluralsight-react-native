4c3b4de84d799aa1411c0846dd4bff3165099523
Tunahan Yilmaz <Tunahan.Yilmaz@logo.com.tr>
2020-05-30 00:44:19 +0300

    created first component for react mobile application.

--- app/views/Home.js
+++ app/views/Home.js
@@ -4,11 +4,13 @@ import { StyleSheet, Text, View } from "react-native";
 export class Home extends React.Component {
 
     render() {
-        <View>
-            <Text> this will be the homepage of my work. </Text>
-            <Text> others line will be here ... </Text>
-            <Text> i can add more thext using Text tag. </Text>
-        </View>
+        return (
+            <View>
+                <Text> this will be the homepage of my work. </Text>
+                <Text> others line will be here ... </Text>
+                <Text> i can add more thext using Text tag. </Text>
+            </View>
+        );
     }
 
 }
