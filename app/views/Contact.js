b8fffde421f0dc1a97bde8c466ea0c820078b5e5
Tunahan Yilmaz <Tunahan.Yilmaz@logo.com.tr>
2020-06-03 21:56:41 +0300

    text inputs, and sumbit example

--- app/views/Contact.js
+++ app/views/Contact.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, TextInput, TouchableHighlight, Alert } from "react-native";
 import { Header } from "./../sections/Header";
+import { StackNavigator } from "react-navigation";
+
 
 export class Contact extends React.Component {
 
@@ -8,13 +10,63 @@ export class Contact extends React.Component {
         header: null,
     }
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            msg: 'enter message',
+            name: 'enter name',
+            email: 'enter your email address',
+        };
+    }
+
+    clearFields = () => {
+        this.setState({
+            msg: '',
+            name: '',
+            email: '',
+        });
+    };
+
+    sendMessage = () => {
+        Alert.alert(this.state.name, this.state.msg);
+        this.props.navigation.goBack();
+    };
+
     render() {
 
         return (
             <View style={styles.container}>
                 <Header message='Press to Login' />
-                <Text style={{flex: 8}}>the contact form will go here</Text>
-                <Text style={{flex: 6}}>more contact form will go here</Text>
+                <Text style={styles.heading}>contact us</Text>
+                <TextInput 
+                    style={styles.inputs}
+                    onChangeText={(_text) => this.setState({name: _text})}
+                    value={this.state.name}
+                />
+                <TextInput 
+                    style={styles.multiInputs}
+                    onChangeText={(_text) => this.setState({msg: _text})}
+                    value={this.state.msg}
+                    multiline={true}
+                    numberOfLines={4}
+                />
+                <TextInput 
+                    style={styles.inputs}
+                    onChangeText={(_text) => this.setState({email: _text})}
+                    value={this.state.email}
+                />
+
+                <TouchableHighlight onPress={this.sendMessage} underlayColor='#31e981'>
+                    <Text style={styles.buttons}>
+                        Send Message
+                    </Text>
+                </TouchableHighlight>
+
+                <TouchableHighlight onPress={this.clearFields} underlayColor='#31e981'>
+                    <Text style={styles.buttons}>
+                        Reset Form
+                    </Text>
+                </TouchableHighlight>
             </View>
         );
 
@@ -26,6 +78,26 @@ const styles = StyleSheet.create({
 
     container: {
         flex: 1,
+        alignItems: 'center',
+        paddingBottom: '45%',
+    },
+    heading: {
+        fontSize: 16,
+        flex: 1,
+    },
+    inputs: {
+        flex: 1,
+        width: '80%',
+        padding: 10,
+    },
+    multiInputs: {
+        flex:2,
+        width: '90%',
+        paddingTop: 20,
+    },
+    buttons: {
+        marginTop: 15,
+        fontSize: 16,
     }
 
 });
